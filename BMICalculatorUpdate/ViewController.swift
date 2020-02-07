37a3a82d9020b20b43e22a14f1f21f74b925ebcd
Andrickson12 <Andrickson12>
2020-02-07 11:45:08 -0500

    Done!

--- BMICalculatorUpdate/ViewController.swift
+++ BMICalculatorUpdate/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Outlets
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSlider(_ sender: UISlider) {
+        
+        let currentValue = String(format: "%.1f", sender.value)
+        
+        heightLabel.text = currentValue
+    }
+    
+    @IBAction func weightSlider(_ sender: UISlider) {
+        let currentValue = String(format: "%.0f", sender.value)
+        
+        weightLabel.text = currentValue
+    }
+    
 }
 
